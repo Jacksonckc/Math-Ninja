d188647ebacf37d6f1dcc8cbedf50592d4b8da41
Brandon-Hagerman <Hag19005@byui.edu>
2022-10-21 14:25:03 -0600

    Added Difficulty Form

--- src/views/GameMode.js
+++ src/views/GameMode.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import ButtonGroup from '@mui/material/ButtonGroup';
 
 const GameMode = () => {
   return (
@@ -15,8 +18,20 @@ const GameMode = () => {
     //   src='https://previews.customer.envatousercontent.com/files/284246327/index.html'
     //   // allowfullscreen
     // ></iframe>
-    <div>GameMode</div>
+
+    <div>
+      <div display='flex' justifyContent='center'>
+        <form>
+          <ButtonGroup variant="contained" orientation='vertical'>
+            <Button onClick={() => (localStorage.setItem("difficulty", "easy"))}>Easy</Button>
+            <Button onClick={() => (localStorage.setItem("difficulty", "medium"))}>Medium</Button>
+            <Button onClick={() => (localStorage.setItem("difficulty", "hard"))}>Hard</Button>
+          </ButtonGroup>
+        </form>
+      </div>
+    </div>
   );
 };
 
 export default GameMode;
+
