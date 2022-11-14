0530af4aace3db74a3f2a52a171b9170df49b55a
Keaton Smith <keaton.smith365@gmail.com>
2022-11-14 13:38:42 -0700

    Removed old code and planned next steps

--- src/game_src/target.js
+++ src/game_src/target.js
@@ -14,36 +14,9 @@ export class Target {
     this.color = this.colorArray[Math.floor(Math.random()*this.colorArray.length)];
 
     this.position = Vector(Math.random()*canvasWidth, canvasHeight + TARGET_SIZE);
-    this.velocity = Vector(vx, vy); // TODO
+    this.velocity = Vector(vx, vy); // TODO Brandon
 
-
-    // if (Math.random() > 0.5) {
-    //   this.x = canvasWidth * Math.random();
-    //   this.y = Math.random() > 0.5 ? -TARGET_SIZE : canvaseHeight;
-    // } else {
-    //   this.x = Math.random() > 0.5 ? -TARGET_SIZE : canvasWidth;
-    //   this.y = canvaseHeight * Math.random();
-    // }
-    
-    // q1 m=+ v=- (-x -y)
-    // q2 m=- v=+ (+x -y)
-    // q3 m=+ v=+ (+x +y)
-    // q4 m=- v=- (-x +y)
-
-    // -x when x > 0
-    // -y when y > 0
-
-//     const negativeX = this.x > (canvasWidth / 2) ? -1 : 1;
-//     const negativeY = this.y > (canvasHeight / 2) ? -1 : 1;
-//     const rise = Math.random() * 100; // Todo
-//     const run = Math.random() * 100; // Todo
-//     const hyp = Math.sqrt((rise**2) + (run**2))
-// ;
-//     // Distance moved per tick (px over ms)
-//     this.deltaX = ((speed / hyp) * run) * (negativeX)
-//     this.deltaY = ((speed / hyp) * rise) * (negativeY)
-
-//     this.active = false
+    this.active = false
   }
 
   draw(ctx) {
@@ -52,10 +25,8 @@ export class Target {
     ctx.fillText(this.value, this.x, this.y)
   }
 
-  tick(timestamp) {
+  tick(ctx, destroyCallback) {
     if (!this.active) return;
-    const deltaTime = timestamp - this.lastUpdate;
-    this.lastUpdate = timestamp
 
     // https://github.com/Kaelinator/AGAD/blob/master/Fruit%20Ninja/Fruit.js
     // Update Position
@@ -66,23 +37,15 @@ export class Target {
     this.velocity.y += GRAVITY;
 
 
-
-    //const GRAVITY = 1; // TODO
-    //this.vy -= GRAVITY; // TODO
-
-    // Move according to velocity
-    // this.x += this.deltaX * deltaTime;  
-    // this.y += this.deltaY * deltaTime;
+    // TODO if y is positive and off screen. destoryCallback(this, isCorrect()) // TODO Keaton
   }
 
-  onClick() {
+  isCorrect() {
     if (!this.active) return;
-    console.log("Implement onClick in Target");
     return this.correct;
   }
 
-  start(timestamp) {
-    this.lastUpdate = timestamp;
+  start() {
     this.active = true;
   }
 }
